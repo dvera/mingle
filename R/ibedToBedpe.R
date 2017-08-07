6732030fa790bb2e267b25fb2ff9b67bd1f48cf6
Daniel Vera <vera@genomics.fsu.edu>
2017-08-07 17:02:04 -0400

    Update ibedToBedpe.R

--- R/ibedToBedpe.R
+++ R/ibedToBedpe.R
@@ -1,24 +1,14 @@
-ibedToBedpe <- function(bedFile , minInteractions=1 , threads=getOption("threads",1L) ){
+ibedToBedpe <- function(bedFile , minInteractions=1 , threads=getOption("threads",1L)  ){
 
   numfiles <- length(bedFile)
-  outnames <- paste0(basename(removeext(
+  if(any(file_ext(bedFile)=="bedpe")){stop("input should be ibed files")}
+  outnames <- paste0(basename(removeext(bedFile)),"_min",minInteractions,".bedpe")
   cmdStrings <- paste(
       "sed 's/[:-]/\t/g ; s/,/\t/g'",bedFile,
-      "| awk '{for(i=1;i<=$7;i++){print $1,$2,$3,$4,$5,$6}}' OFS='\t'",
-      " | awk '{",
-        "if($1==$4){",
-          "left=int($2/",binsize,");",
-          "right=int($6/",binsize,");",
-          "if(left<right){print $1,left,right}",
-          "else{print $1,right,left}",
-        "}}' OFS='\t' | sort -T . -S 10G -k1,1 -k2,2n -k3,3n | uniq -c | awk '{",
-          "if($1>=",minInteractions,"){",
-            "print $2,$3,$4,$1",
-          "}",
-        "}' OFS='\t'"
+        "| awk '{if(sqrt(($7)^2)>=",minInteractions,"){for(i=1;i<=$7;i++){a++;print $1,$2,$3,$4,$5,$6,a,$5-$2}}}' OFS='\t' >",outnames
     ) 
 
-  res <- cmdRun(cmdStrings,threads=threads,tsv=T)
+  res <- cmdRun(cmdStrings,threads=threads)
   
-  return(res)
+  return(outnames)
 }
